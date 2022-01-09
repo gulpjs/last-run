50a17d874923dafc5c00fbfff23c935424c79df0
Takayuki Sato <sttk.xslet@gmail.com>
2022-01-10 08:58:21 +0900

    chore!: Normalize repository, dropping node <10.13 support (#8)
    feat: Support non-extensible functions by removing WeakMap shim
    feat: Remove default-resolution dependency since platform has consistent resolution

--- test/last-run.js
+++ test/last-run.js
@@ -2,71 +2,75 @@
 
 var expect = require('expect');
 
-var defaultResolution = require('default-resolution');
-
 var lastRun = require('../');
 
-describe('lastRun', function() {
+describe('lastRun', function () {
 
   var since;
 
-  beforeEach(function(done) {
+  beforeEach(function (done) {
     since = Date.now();
-    // Account for default resolution
-    since = since - (since % defaultResolution());
     done();
   });
 
-  it('should record function capture time', function(done) {
-    function test() {}
+  it('should record function capture time', function (done) {
+    function test() { }
+
+    var beforeRun = Date.now();
 
     lastRun.capture(test);
 
-    expect(lastRun(test)).toExist();
-    expect(lastRun(test)).toBeLessThanOrEqualTo(Date.now());
+    var justRun = lastRun(test);
+    var afterRun = Date.now();
+    expect(justRun).toBeGreaterThanOrEqual(beforeRun);
+    expect(justRun).toBeLessThanOrEqual(afterRun);
     done();
   });
 
-  it('should accept a timestamp', function(done) {
-    function test() {}
+  it('should accept a timestamp', function (done) {
+    function test() { }
 
     lastRun.capture(test, since);
 
-    expect(lastRun(test)).toExist();
     expect(lastRun(test)).toEqual(since);
     done();
   });
 
-  it('removes last run time with release method', function(done) {
-    function test() {}
+  it('removes last run time with release method', function (done) {
+    function test() { }
+
+    var beforeRun = Date.now();
 
     lastRun.capture(test);
 
-    expect(lastRun(test)).toExist();
+    var justRun = lastRun(test);
+    var afterRun = Date.now();
+    expect(justRun).toBeGreaterThanOrEqual(beforeRun);
+    expect(justRun).toBeLessThanOrEqual(afterRun);
 
     lastRun.release(test);
 
-    expect(lastRun(test)).toNotExist();
+    expect(lastRun(test)).toBeUndefined();
     done();
   });
 
-  it('does not error on release if not captures', function(done) {
-    function test() {}
+  it('does not error on release if not captures', function (done) {
+    function test() { }
 
     lastRun.release(test);
 
-    expect(lastRun(test)).toNotExist();
+    expect(lastRun(test)).toBeUndefined();
     done();
   });
 
-  it('should return undefined for a function not captured', function(done) {
-    function test() {}
+  it('should return undefined for a function not captured', function (done) {
+    function test() { }
 
-    expect(lastRun(test)).toNotExist();
+    expect(lastRun(test)).toBeUndefined();
     done();
   });
 
-  it('should throw on non-functions', function(done) {
+  it('should throw on non-functions', function (done) {
     function obj() {
       lastRun({});
     }
@@ -95,79 +99,51 @@ describe('lastRun', function() {
     done();
   });
 
-  it('works with anonymous functions', function(done) {
-    var test = function() {};
+  it('works with anonymous functions', function (done) {
+    var test = function () { };
+
+    var beforeRun = Date.now();
 
     lastRun.capture(test);
 
-    expect(lastRun(test)).toExist();
-    expect(lastRun(test)).toBeLessThanOrEqualTo(Date.now());
+    var justRun = lastRun(test);
+    var afterRun = Date.now();
+    expect(justRun).toBeGreaterThanOrEqual(beforeRun);
+    expect(justRun).toBeLessThanOrEqual(afterRun);
     done();
   });
 
-  it('should give time with 1s resolution', function(done) {
+  it('should give time with 1s resolution', function (done) {
     var resolution = 1000; // 1s
     since = Date.now();
     since = since - (since % resolution);
 
-    function test() {}
+    function test() { }
     lastRun.capture(test);
 
     expect(lastRun(test, resolution)).toEqual(since);
     done();
   });
 
-  it('should accept a string for resolution', function(done) {
+  it('should accept a string for resolution', function (done) {
     var resolution = '1000'; // 1s
     since = Date.now();
     since = since - (since % 1000);
 
-    function test() {}
+    function test() { }
     lastRun.capture(test);
 
     expect(lastRun(test, resolution)).toEqual(since);
     done();
   });
 
-  it('should use default resolution when forced to 0ms resolution', function(done) {
+  it('should use default resolution when forced to 0ms resolution', function (done) {
     var resolution = 0;
 
-    function test() {}
+    function test() { }
     lastRun.capture(test);
 
     expect(lastRun(test, resolution)).toEqual(since);
-    done();
-  });
-
-  it('throws on non-enumerable functions when using weakmap shim', function(done) {
-
-    function extensions() {
-      var test = function() {};
-      Object.preventExtensions(test);
-      lastRun.capture(test);
-    }
-
-    function seal() {
-      var test = function() {};
-      Object.seal(test);
-      lastRun.capture(test);
-    }
-
-    function freeze() {
-      var test = function() {};
-      Object.freeze(test);
-      lastRun.capture(test);
-    }
-
-    if (/v0.10/.test(process.version)) {
-      expect(extensions).toThrow('Only extensible functions can be captured');
-      expect(seal).toThrow('Only extensible functions can be captured');
-      expect(freeze).toThrow('Only extensible functions can be captured');
-    } else {
-      expect(extensions).toNotThrow();
-      expect(seal).toNotThrow();
-      expect(freeze).toNotThrow();
-    }
     done();
   });
 
